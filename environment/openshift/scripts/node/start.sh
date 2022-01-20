3661e80e622cc61da76ad57b1b56ab8d01de5e62
ataberkozek <ataberk.r.ozek@gmail.com>
2022-01-20 14:18:46 +0300

    trying to get ips from ip list ID-11
    on-behalf-of: @ayrotek <ataberk.ozek@ayrotek.com.tr>

--- environment/openshift/scripts/node/start.sh
+++ environment/openshift/scripts/node/start.sh
@@ -15,25 +15,25 @@ echo
 
 echo $ips
 echo "TEST ips"
-echo ${NODE_IP_LIST[1]}
-echo "TEST node ip 1"
+myarray=(`echo $ips | tr ',' ' '`)
+echo "My array: ${myarray[@]}"
 
-a=""
+
 
 if [[ ${NODE_NAME} == "Node1" ]]; then
-    a = "${NODE_IP_LIST[1]}"
+    a="${myarray[1]}"
 elif [[ ${NODE_NAME} == "Node2" ]]; then
-    a = "${NODE_IP_LIST[2]}"
+    a="${myarray[2]}"
 elif [[ ${NODE_NAME} == "Node3" ]]; then
-    a = "${NODE_IP_LIST[3]}"
+    a="${myarray[3]}"
 elif [[ ${NODE_NAME} == "Node4" ]]; then
-    a = "${NODE_IP_LIST[4]}"
+    a="${myarray[4]}"
 fi
 
-echo $NODE_IP
+echo $a
 echo "Starting indy-node service ..."
-echo "/usr/bin/env python3 -O /usr/local/bin/start_indy_node ${NODE_NAME} $NODE_IP ${NODE_PORT} $NODE_IP ${CLIENT_PORT}"
+echo "/usr/bin/env python3 -O /usr/local/bin/start_indy_node ${NODE_NAME} $a ${NODE_PORT} $a ${CLIENT_PORT}"
 
-exec /usr/bin/env python3 -O /usr/local/bin/start_indy_node ${NODE_NAME} $NODE_IP ${NODE_PORT} $NODE_IP ${CLIENT_PORT}
+exec /usr/bin/env python3 -O /usr/local/bin/start_indy_node ${NODE_NAME} $a ${NODE_PORT} $a ${CLIENT_PORT}
 
 # echo "Indy node started."
